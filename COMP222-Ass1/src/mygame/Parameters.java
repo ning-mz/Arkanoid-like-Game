e409a11aeab8f5ac49c515099eea6c4c9f1c0c09
M.N <30820136+ning-mz@users.noreply.github.com>
2019-03-28 15:20:32 +0000

    Release

--- COMP222-Ass1/src/mygame/Parameters.java
+++ COMP222-Ass1/src/mygame/Parameters.java
@@ -5,6 +5,7 @@
  */
 package mygame;
 
+import com.jme3.audio.AudioNode;
 import com.jme3.math.Vector3f;
 /**
  *
@@ -12,13 +13,39 @@ import com.jme3.math.Vector3f;
  */
 public class Parameters {
     
-   public Vector3f[] targetLocation = new Vector3f[9];
+   private Vector3f[] targetLocation = new Vector3f[9];
+   private AudioNode board, target, next, failed, win;
+   
+  
    
    
-   public Vector3f[] getLocationsOfTarget(int level){
-     
+   public Vector3f[] getLocationsOfTarget(int level){    
    if (level==1){ 
        //level 1 
+       targetLocation[0] = new Vector3f(10, 25, 1.9f);
+       targetLocation[1] = new Vector3f(20, 25, 1.9f);
+       targetLocation[2] = new Vector3f(4, 17, 1.9f);
+       targetLocation[3] = new Vector3f(26, 17, 1.9f);
+       targetLocation[4] = new Vector3f(6, 6, 1.9f);
+       targetLocation[5] = new Vector3f(24, 6, 1.9f);
+       targetLocation[6] = new Vector3f(15, 20, 1.9f);
+       targetLocation[7] = new Vector3f(15, 15, 1.9f);
+       targetLocation[8] = new Vector3f(15, 8, 1.9f);  
+       return targetLocation;
+   }else if (level==2){
+       //level 2
+       targetLocation[0] = new Vector3f(11, 16, 1.9f);
+       targetLocation[1] = new Vector3f(15, 16, 1.9f);
+       targetLocation[2] = new Vector3f(19, 16, 1.9f);
+       targetLocation[3] = new Vector3f(4, 20, 1.9f);
+       targetLocation[4] = new Vector3f(15, 20, 1.9f);
+       targetLocation[5] = new Vector3f(26, 20, 1.9f);
+       targetLocation[6] = new Vector3f(11, 24, 1.9f);
+       targetLocation[7] = new Vector3f(15, 24, 1.9f);
+       targetLocation[8] = new Vector3f(19, 24, 1.9f); 
+       return targetLocation;
+   }else{
+       //level 3
        targetLocation[0] = new Vector3f(7, 16, 1.9f);
        targetLocation[1] = new Vector3f(15, 16, 1.9f);
        targetLocation[2] = new Vector3f(23, 16, 1.9f);
@@ -27,33 +54,8 @@ public class Parameters {
        targetLocation[5] = new Vector3f(23, 20, 1.9f);
        targetLocation[6] = new Vector3f(7, 24, 1.9f);
        targetLocation[7] = new Vector3f(15, 24, 1.9f);
-       targetLocation[8] = new Vector3f(23, 24, 1.9f);  
-       return targetLocation;
-   }else if (level==2){
-       //level 2
-       targetLocation[0] = new Vector3f(11, 16, 1.9f);
-       targetLocation[1] = new Vector3f(15, 16, 1.9f);
-       targetLocation[2] = new Vector3f(19, 16, 1.9f);
-       targetLocation[3] = new Vector3f(3, 20, 1.9f);
-       targetLocation[4] = new Vector3f(15, 20, 1.9f);
-       targetLocation[5] = new Vector3f(27, 20, 1.9f);
-       targetLocation[6] = new Vector3f(11, 24, 1.9f);
-       targetLocation[7] = new Vector3f(15, 24, 1.9f);
-       targetLocation[8] = new Vector3f(19, 24, 1.9f); 
-       return targetLocation;
-   }else{
-       //level 3
-       targetLocation[0] = new Vector3f(4, 20, 1.9f);
-       targetLocation[1] = new Vector3f(8, 20, 1.9f);
-       targetLocation[2] = new Vector3f();
-       targetLocation[3] = new Vector3f();
-       targetLocation[4] = new Vector3f();
-       targetLocation[5] = new Vector3f();
-       targetLocation[6] = new Vector3f();
-       targetLocation[7] = new Vector3f();
-       targetLocation[8] = new Vector3f();
+       targetLocation[8] = new Vector3f(23, 24, 1.9f);
        return targetLocation;
     }  
-  }
-    
+  }  
 }
--- COMP222-Ass1/src/mygame/game.java
+++ COMP222-Ass1/src/mygame/game.java
@@ -6,6 +6,7 @@
 package mygame;
 import mygame.Parameters.*;
 import com.jme3.app.SimpleApplication;
+import com.jme3.audio.AudioNode;
 import com.jme3.audio.Environment;
 import com.jme3.scene.Spatial;
 import com.jme3.scene.Node;
@@ -23,11 +24,14 @@ import com.jme3.math.ColorRGBA;
 import com.jme3.math.FastMath;
 import com.jme3.math.Vector3f;
 import com.jme3.scene.debug.Arrow;
+import com.jme3.ui.Picture;
+import java.util.Timer;
+import java.util.TimerTask;
 
 
 /**
  *
- * @author 10141
+ * @author Maizhen Ning
  */
 public class game extends SimpleApplication{
     //parameters of background
@@ -42,19 +46,24 @@ public class game extends SimpleApplication{
     public static final float ARROW_HALF_MIN = FastMath.PI /12 ;
     private Spatial boundary;
     private Spatial board;
+    private Spatial board1;
+    private Spatial board2;
+    private Spatial board3;
     private Geometry arrow;
-    
-    
+    private AudioNode boardAudio, targetAudio, nextAudio, failedAudio, winAudio;
+    private Picture picture= new Picture("Picture");
+    private Picture pictureWin = new Picture("PictureWin");
     private AmbientLight ambientLight;
     private DirectionalLight directionalLight;
     
 
     private final Node targetNode = new Node("Targets");
+
     
     //properties of ball
     private Vector3f ballDirection;
-    private final float ballSpeed = 8f;
-    private float boardMoveSpeed = 10f;
+    private float ballSpeed;
+    private float boardMoveSpeed;
     private Vector3f[] targetLocations;
     private int level;
     private Parameters parameters= new Parameters(); 
@@ -71,14 +80,77 @@ public class game extends SimpleApplication{
     private boolean running = false;
     private boolean leftKeyFlag = true;
     private boolean rightKeyFlag = true;
+    private boolean welcome = true;
+    
+        private class analogControl implements AnalogListener{
+        @Override
+        public void onAnalog(String name, float value, float tpf) {                       
+            if (!start){
+                if (name.equals("Left")){
+                    Float angle = arrow.getUserData("angle");
+                    angle = (angle > ARROW_HALF_MAX && angle < (FastMath.PI - ARROW_HALF_MAX)) ? FastMath.PI - ARROW_HALF_MAX : angle;
+                    angle = (angle >= (FastMath.PI - ARROW_HALF_MIN)) ? FastMath.PI - ARROW_HALF_MIN : angle + tpf * FastMath.QUARTER_PI;
+                    ((Arrow) arrow.getMesh()).setArrowExtent(new Vector3f(ARROW_LENGTH * FastMath.cos(angle), ARROW_LENGTH * FastMath.sin(angle), 0));
+                    arrow.setUserData("angle", angle); 
+                    
+                }else if (name.equals("Right")){
+                    Float angle = arrow.getUserData("angle");
+                    angle = (angle > ARROW_HALF_MAX && angle < (FastMath.PI - ARROW_HALF_MAX)) ? ARROW_HALF_MAX : angle;
+                    angle = (angle <= ARROW_HALF_MIN) ? ARROW_HALF_MIN : angle - tpf * FastMath.QUARTER_PI;
+                    ((Arrow) arrow.getMesh()).setArrowExtent(new Vector3f(ARROW_LENGTH * FastMath.cos(angle), ARROW_LENGTH * FastMath.sin(angle), 0));
+                    arrow.setUserData("angle", angle);               
+                }
+            }else if(start && running){               
+                if (name.equals("Left")){                   
+                    board.move(Vector3f.UNIT_X.mult(-boardMoveSpeed * tpf));
+                }else if (name.equals("Right")){
+                        board.move(Vector3f.UNIT_X.mult(boardMoveSpeed * tpf));
+                } 
+           }
+          //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.       
+        }      
+    }
+    
+    private class actionControl implements ActionListener{
+        @Override
+        public void onAction(String name, boolean isPressed, float tpf) {           
+            if (welcome == true){ 
+                if (name.equals("Space")){
+                    welcome = false;
+                    rootNode.detachChild(picture);     
+                    setAllKeys(true);
+                }
+                return;
+            }
+            if (!start){
+                if (name.equals("Up")){
+                    Float angle = arrow.getUserData("angle");
+                    //ballDirection = new Vector3f(ARROW_LENGTH * FastMath.cos(angle),  ARROW_LENGTH * FastMath.sign(angle), 0).normalize();
+                    start = true;
+                    running = true;
+                    ballDirection = new Vector3f(ARROW_LENGTH * FastMath.cos(angle), ARROW_LENGTH * FastMath.sin(angle), 0).normalize();
+                    rootNode.detachChild(arrow);
+                }           
+            }else{
+                if (isPressed && name.equals("Up")){
+                    running = !running;  
+                }
+            }         
+            //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }        
+    }
+    
+    
+    private final analogControl boardControl = new analogControl();
+    private final actionControl otherControl = new actionControl();
     
     
     public void startGame(){
         if (start)
             return;
-        level =1;
+        level = 1;
         setEnvironment(level, true); 
-        setAllKeys(true);
+        //setAllKeys(true);
     }
     
     public void setEnvironment(int level, boolean firstTimeFlag){
@@ -91,13 +163,19 @@ public class game extends SimpleApplication{
             rootNode.attachChild(boundary);       
         
             //set board
-            board = assetManager.loadModel("Models/Board2.j3o");
+            board1 = assetManager.loadModel("Models/Board1.j3o");
+            board2 = assetManager.loadModel("Models/Board2.j3o");
+            board3 = assetManager.loadModel("Models/Board3.j3o");
             Material matForBoard = new Material(assetManager, "Common/MatDefs/Light/Lighting.j3md");
             matForBoard.setTexture("DiffuseMap", assetManager.loadTexture("Textures/board.jpg"));
-            board.setLocalTranslation(13, 0, 3); 
-            board.setMaterial(matForBoard);
+            board1.setLocalTranslation(11.5f, 0, 3); 
+            board1.setMaterial(matForBoard);
+            board2.setMaterial(matForBoard);
+            board3.setMaterial(matForBoard);
+            board = board1;
             rootNode.attachChild(board);                                        
-        
+            boardMoveSpeed = 10f;
+            
             //set ball
             ball = new Geometry("ball", new Sphere(50, 50, 1f));
             Material matForBall = new Material(assetManager, "Common/MatDefs/Light/Lighting.j3md");
@@ -105,6 +183,7 @@ public class game extends SimpleApplication{
             ball.setMaterial(matForBall);               
             ball.setLocalTranslation(15, 2f, 1.9f);
             rootNode.attachChild(ball);
+            ballSpeed = 13f;
             
             //set lights
             ambientLight = new AmbientLight();
@@ -113,18 +192,54 @@ public class game extends SimpleApplication{
             directionalLight.setColor(ColorRGBA.White.mult(.5f));
             directionalLight.setDirection(Vector3f.UNIT_Z.negate());
             rootNode.addLight(ambientLight);
-            rootNode.addLight(directionalLight);      
+            rootNode.addLight(directionalLight);   
+            
+            //set audio sounds
+            boardAudio = new AudioNode(assetManager, "Sounds/board.ogg");            
+            targetAudio = new AudioNode(assetManager, "Sounds/target.ogg");
+            failedAudio = new AudioNode(assetManager, "Sounds/failed.ogg");
+            nextAudio = new AudioNode(assetManager, "Sounds/next.ogg");
+            winAudio = new AudioNode(assetManager, "Sounds/win.ogg");
+            boardAudio.setPositional(false);        
+            targetAudio.setPositional(false);
+            failedAudio.setPositional(false);
+            nextAudio.setPositional(false);
+            winAudio.setPositional(false);
+            
+            //set picture for interface
+            picture.setPosition(0, 0);
+            picture.setImage(assetManager, "Interfaces/welcome.png", false);
+            picture.setWidth(settings.getWidth());
+            picture.setHeight(settings.getHeight());
+            rootNode.attachChild(picture);
+            inputManager.addMapping("Space", new KeyTrigger(KeyInput.KEY_SPACE));
+            inputManager.addListener(otherControl, "Space"); 
             
         }else if((level == 1) && (firstTimeFlag == false)){
+            rootNode.detachChild(board);
             ball.setLocalTranslation(15, 2f, 1.9f);
-            board.setLocalTranslation(13, 0, 3);
+            board = board1;
+            board.setLocalTranslation(11.5f, 0, 3);
             targetNode.detachAllChildren();
+            rootNode.attachChild(board);
+            ballSpeed = 14f;
+            boardMoveSpeed = 10f;
         }else if(level == 2){
+            rootNode.detachChild(board);
             ball.setLocalTranslation(15, 2f, 1.9f);
-            board.setLocalTranslation(13, 0, 3);       
+            board = board2;
+            board.setLocalTranslation(12.25f, 0, 3);   
+            rootNode.attachChild(board);
+            ballSpeed = 18f;
+            boardMoveSpeed = 12f;
         }else if(level == 3){
+            rootNode.detachChild(board);
             ball.setLocalTranslation(15, 2f, 1.9f);
-            board.setLocalTranslation(13, 0, 3);   
+            board = board3;
+            board.setLocalTranslation(13, 0, 3);  
+            rootNode.attachChild(board);      
+            ballSpeed = 20f;
+            boardMoveSpeed = 14f;
         }
         
         //set targets
@@ -145,92 +260,23 @@ public class game extends SimpleApplication{
         arrow = new Geometry("arrow", new Arrow(
                     new Vector3f(3 * FastMath.cos(FastMath.QUARTER_PI), 3 * FastMath.sin(FastMath.QUARTER_PI), 0)));
         Material matForArrow = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
-        matForArrow.setColor("Color", ColorRGBA.Blue);
+        matForArrow.setColor("Color", ColorRGBA.Green);
         arrow.setMaterial(matForArrow);
         arrow.move(15, 1.5f, 1.5f);
         arrow.setUserData("angle", FastMath.QUARTER_PI);
         ((Arrow) arrow.getMesh()).setArrowExtent(new Vector3f(ARROW_LENGTH * FastMath.cos(FastMath.QUARTER_PI), ARROW_LENGTH * FastMath.sin(FastMath.QUARTER_PI), 0));
-        rootNode.attachChild(arrow);
-        
-        
-        
-        
-        
+        rootNode.attachChild(arrow);       
     } 
     
     
-    private class analogControl implements AnalogListener{
-        @Override
-        public void onAnalog(String name, float value, float tpf) {                       
-            if (!start){
-                if (name.equals("Left")){
-                    Float angle = arrow.getUserData("angle");
-                    angle = (angle > ARROW_HALF_MAX && angle < (FastMath.PI - ARROW_HALF_MAX)) ? FastMath.PI - ARROW_HALF_MAX : angle;
-                    angle = (angle >= (FastMath.PI - ARROW_HALF_MIN)) ? FastMath.PI - ARROW_HALF_MIN : angle + tpf * FastMath.QUARTER_PI;
-                    ((Arrow) arrow.getMesh()).setArrowExtent(new Vector3f(ARROW_LENGTH * FastMath.cos(angle), ARROW_LENGTH * FastMath.sin(angle), 0));
-                    arrow.setUserData("angle", angle); 
-                    
-                }else if (name.equals("Right")){
-                    Float angle = arrow.getUserData("angle");
-                    angle = (angle > ARROW_HALF_MAX && angle < (FastMath.PI - ARROW_HALF_MAX)) ? ARROW_HALF_MAX : angle;
-                    angle = (angle <= ARROW_HALF_MIN) ? ARROW_HALF_MIN : angle - tpf * FastMath.QUARTER_PI;
-                    ((Arrow) arrow.getMesh()).setArrowExtent(new Vector3f(ARROW_LENGTH * FastMath.cos(angle), ARROW_LENGTH * FastMath.sin(angle), 0));
-                    arrow.setUserData("angle", angle);               
-                }
-            }else if(start){           
-                if (name.equals("Left")){                   
-                    board.move(Vector3f.UNIT_X.mult(-boardMoveSpeed * tpf));
-                }else if (name.equals("Right")){
-                        board.move(Vector3f.UNIT_X.mult(boardMoveSpeed * tpf));
-                }           
-            }
-            
-            
-          //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-       
-        
-        }      
-    }
-    
-    private class actionControl implements ActionListener{
-        @Override
-        public void onAction(String name, boolean isPressed, float tpf) {
-            if (!start){
-                if (name.equals("Up")){
-                    Float angle = arrow.getUserData("angle");
-                    //ballDirection = new Vector3f(ARROW_LENGTH * FastMath.cos(angle),  ARROW_LENGTH * FastMath.sign(angle), 0).normalize();
-                    start = true;
-                    running = true;
-                    ballDirection = new Vector3f(ARROW_LENGTH * FastMath.cos(angle), ARROW_LENGTH * FastMath.sin(angle), 0).normalize();
-                    rootNode.detachChild(arrow);
-                }
-            
-            }else{
-                if (isPressed && name.equals("Up")){
-                    running = !running;  
-                    
-                }
-            
-            
-            }
-            
-            
-            
-            
-            //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-        }        
-    }
-    
-    
-    private final analogControl boardControl = new analogControl();
-    private final actionControl otherControl = new actionControl();
+
     
     public void setAllKeys(boolean flag){
         if (flag == true){       
             inputManager.addMapping("Left", new KeyTrigger(KeyInput.KEY_LEFT));
             inputManager.addMapping("Right", new KeyTrigger(KeyInput.KEY_RIGHT));
             inputManager.addMapping("Up", new KeyTrigger(KeyInput.KEY_UP));
-            inputManager.addMapping("Escape", new KeyTrigger(KeyInput.KEY_ESCAPE));
+            inputManager.addMapping("Escape", new KeyTrigger(KeyInput.KEY_ESCAPE));            
             inputManager.addListener(boardControl, "Left", "Right");
             inputManager.addListener(otherControl, "Up", "Escape");
         }
@@ -281,64 +327,90 @@ public class game extends SimpleApplication{
     @Override
     public void simpleUpdate(float tpf){
         //watching state of game
-       if (!running)
-            return;      
-        
+        if (welcome == true)
+            return;        
+        if (!running)
+            return;             
         if (!start)
             return;     
-        
-        
+       
         //collision with boundary      
         if(ball.getLocalTranslation().x <= LEFT_BOUNDARY){
-            ballDirection.x = FastMath.abs(ballDirection.x);           
+            if (ballDirection.x < 0){
+                ballDirection.x = -(ballDirection.x); 
+                boardAudio.playInstance();
+            }          
         }else if(ball.getLocalTranslation().x >= RIGHT_BOUNDARY){
-            ballDirection.x = -FastMath.abs(ballDirection.x);           
+            if (ballDirection.x > 0){
+                ballDirection.x = -(ballDirection.x);   
+                boardAudio.playInstance();
+            }        
         }else if(ball.getLocalTranslation().y >= UPPER_BOUNDARY){
-            ballDirection.y = -FastMath.abs(ballDirection.y);        
+            if (ballDirection.y > 0){
+                ballDirection.y = -(ballDirection.y);       
+                boardAudio.playInstance();
+            } 
         }
-        
-        
-        
+      
         //collision with targets       
         CollisionResults targetCollision = new CollisionResults();
         for (Spatial target : targetNode.getChildren()){
             target.collideWith(ball.getWorldBound(), targetCollision);
             if (targetCollision.size() > 0){
                 //targetCollision.clear();           
-                targetNode.setUserData("delete", target);
-                targetNode.setUserData("vector", target.getLocalTranslation().subtract(ball.getLocalTranslation()));
+                targetNode.setUserData("remove", target);
+                targetNode.setUserData("direction", target.getLocalTranslation().subtract(ball.getLocalTranslation()));
+                System.out.println("Collision on target");
                 break;
             }
         }
-        if (targetNode.getUserData("delete") != null){
-            targetNode.detachChild((Spatial)targetNode.getUserData("delete"));
-            Vector3f collideVector = targetNode.getUserData("vector");
+        if (targetNode.getUserData("remove") != null){
+            targetAudio.playInstance();
+            targetNode.detachChild((Spatial)targetNode.getUserData("remove"));
+            Vector3f collideVector = targetNode.getUserData("direction");
             ballDirection = collideVector.mult((ballDirection.dot(collideVector) * 2 ) / collideVector.dot(collideVector)).subtract(ballDirection).negate();
-            targetNode.setUserData("delete", null);
-            targetNode.setUserData("vector", null);
+            targetNode.setUserData("remove", null);
+            targetNode.setUserData("direction", null);
         } 
          
         //collision with board
         CollisionResults boardCollision = new CollisionResults();
         board.collideWith(ball.getWorldBound(), boardCollision);
         if (boardCollision.size() > 0){
-            ballDirection.y = -ballDirection.y;
-            System.out.println("Collision on board");
+            if (ballDirection.y < 0){
+                ballDirection.y = -ballDirection.y;
+                boardAudio.playInstance();
+                System.out.println("Collision on board");
+            }
         }
         
         //watching is empty of targets
         if (targetNode.getChildren().isEmpty()){
             if (level < 3){
-                level++;
+                nextAudio.playInstance();
+                level++;       
                 start = false;
                 running = false;
                 setEnvironment(level, false);
+                return;
+            }else if(level == 3){
+                winAudio.playInstance();
+                start = false;
+                running = false;
+                level = 1;
+                
+                pictureWin.setPosition(0, 0);
+                pictureWin.setImage(assetManager, "Interfaces/finish.png", false);
+                pictureWin.setWidth(settings.getWidth());
+                pictureWin.setHeight(settings.getHeight());
+                rootNode.attachChild(pictureWin);
                 return;
             }             
         }
             
         //watching is ball fall
-        if (ball.getLocalTranslation().y < LOWER_BOUNDARY){           
+        if (ball.getLocalTranslation().y < LOWER_BOUNDARY){   
+            failedAudio.playInstance();
             level = 1;
             start = false;
             running = false;
@@ -367,7 +439,7 @@ public class game extends SimpleApplication{
             }
         }
         
-        
+        //watching collision with obstacles
         
         
         ball.move(ballDirection.mult(ballSpeed * tpf));
